3e2aed71546d92681cb4c7ef0a3e6e4e2fa0ff45
Nicolas Manns <nicolas.manns@gmail.com>
2026-01-25 16:55:17 +0100

    basic signinform for test (TODO rework it)

--- src/components/ui/FullScreenMessage.tsx
+++ src/components/ui/FullScreenMessage.tsx
@@ -4,8 +4,8 @@ type FullScreenMessageProps = {
 
 export function FullScreenMessage({ message }: FullScreenMessageProps) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-slate-50">
-      <div className="text-slate-300">{message}</div>
+    <div className="min-h-screen flex items-center justify-center bg-slate-50 text-slate-900">
+      <div className="text-slate-900">{message}</div>
     </div>
   );
 }
