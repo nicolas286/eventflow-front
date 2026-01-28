791bbce1e14b666e1a88351069eede0f198c0cf2
Nicolas Manns <nicolas.manns@gmail.com>
2026-01-28 13:39:11 +0100

    commentaires dans adminsingleeventpage

--- src/pages/admin/AdminSingleEventPage.tsx
+++ src/pages/admin/AdminSingleEventPage.tsx
@@ -1,3 +1,11 @@
+
+// Onglets : 
+// - Détails + formulaire full size pour modifier
+// - Tickets + modification, création, suppression produits
+// - Formulaire d'inscription (mini builder en liste pour les formulaires)
+// - Participants + modification, création participant, marqué comme payé, export CSV/Excel : vue des participants 
+// commandes en tableau scrollable comme excel (ou autre système simple)
+
 export function AdminSingleEventPage() {
   return (
     <div className="adminCard">
