afee9dcc6cc6ee81559b63a9df4c57626c69f781
Nicolas Manns <nicolas.manns@gmail.com>
2026-02-18 10:00:53 +0100

    build error

--- src/pages/admin/OnboardingWizard.tsx
+++ src/pages/admin/OnboardingWizard.tsx
@@ -1,8 +1,7 @@
 import { useMemo, useState } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 
-import "../../styles/desktop/onboardingWizard.desktop.css";
-import "../../styles/mobile/onboardingWizard.mobile.css";
+import "../../styles/desktop/OnboardingWizard.desktop.css";
 
 import type { AdminOutletContext } from "../admin/AdminDashboard";
 import { supabase } from "../../gateways/supabase/supabaseClient";
